539cefd12a87f8661834860041de0450de640c98
여름 <resumm.er09@gmail.com>
2023-03-23 21:02:09 +0900

    0323

---
+++ open0322/Image_window.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace open0322
+{
+    partial class Image_win
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picOriginal = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.열기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuOpenImg = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.picResult = new System.Windows.Forms.PictureBox();
+            this.btnBlur = new System.Windows.Forms.Button();
+            this.btnGray = new System.Windows.Forms.Button();
+            this.btnBin = new System.Windows.Forms.Button();
+            this.btnEdge = new System.Windows.Forms.Button();
+            this.btnTest1 = new System.Windows.Forms.Button();
+            this.txtReadbox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picOriginal)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picOriginal
+            // 
+            this.picOriginal.Location = new System.Drawing.Point(39, 155);
+            this.picOriginal.Name = "picOriginal";
+            this.picOriginal.Size = new System.Drawing.Size(800, 800);
+            this.picOriginal.TabIndex = 0;
+            this.picOriginal.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.열기ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1904, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 열기ToolStripMenuItem
+            // 
+            this.열기ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuOpenImg});
+            this.열기ToolStripMenuItem.Name = "열기ToolStripMenuItem";
+            this.열기ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.열기ToolStripMenuItem.Text = "열기";
+            // 
+            // MenuOpenImg
+            // 
+            this.MenuOpenImg.Name = "MenuOpenImg";
+            this.MenuOpenImg.Size = new System.Drawing.Size(110, 22);
+            this.MenuOpenImg.Text = "이미지";
+            this.MenuOpenImg.Click += new System.EventHandler(this.MenuOpenImg_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(904, 594);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "초기화";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // picResult
+            // 
+            this.picResult.Location = new System.Drawing.Point(1044, 155);
+            this.picResult.Name = "picResult";
+            this.picResult.Size = new System.Drawing.Size(800, 800);
+            this.picResult.TabIndex = 4;
+            this.picResult.TabStop = false;
+            // 
+            // btnBlur
+            // 
+            this.btnBlur.Location = new System.Drawing.Point(904, 193);
+            this.btnBlur.Name = "btnBlur";
+            this.btnBlur.Size = new System.Drawing.Size(75, 23);
+            this.btnBlur.TabIndex = 5;
+            this.btnBlur.Text = "가우시안 블러";
+            this.btnBlur.UseVisualStyleBackColor = true;
+            this.btnBlur.Click += new System.EventHandler(this.btnBlur_Click);
+            // 
+            // btnGray
+            // 
+            this.btnGray.Location = new System.Drawing.Point(904, 231);
+            this.btnGray.Name = "btnGray";
+            this.btnGray.Size = new System.Drawing.Size(75, 23);
+            this.btnGray.TabIndex = 6;
+            this.btnGray.Text = "흑백화";
+            this.btnGray.UseVisualStyleBackColor = true;
+            this.btnGray.Click += new System.EventHandler(this.btnGray_Click);
+            // 
+            // btnBin
+            // 
+            this.btnBin.Location = new System.Drawing.Point(904, 273);
+            this.btnBin.Name = "btnBin";
+            this.btnBin.Size = new System.Drawing.Size(75, 23);
+            this.btnBin.TabIndex = 7;
+            this.btnBin.Text = "이진화";
+            this.btnBin.UseVisualStyleBackColor = true;
+            this.btnBin.Click += new System.EventHandler(this.btnBin_Click);
+            // 
+            // btnEdge
+            // 
+            this.btnEdge.Location = new System.Drawing.Point(904, 312);
+            this.btnEdge.Name = "btnEdge";
+            this.btnEdge.Size = new System.Drawing.Size(75, 23);
+            this.btnEdge.TabIndex = 8;
+            this.btnEdge.Text = "경계추출";
+            this.btnEdge.UseVisualStyleBackColor = true;
+            this.btnEdge.Click += new System.EventHandler(this.btnEdge_Click);
+            // 
+            // btnTest1
+            // 
+            this.btnTest1.Location = new System.Drawing.Point(904, 351);
+            this.btnTest1.Name = "btnTest1";
+            this.btnTest1.Size = new System.Drawing.Size(75, 23);
+            this.btnTest1.TabIndex = 9;
+            this.btnTest1.Text = "글자인식";
+            this.btnTest1.UseVisualStyleBackColor = true;
+            this.btnTest1.Click += new System.EventHandler(this.btnTest1_Click);
+            // 
+            // txtReadbox
+            // 
+            this.txtReadbox.Location = new System.Drawing.Point(1044, 107);
+            this.txtReadbox.Name = "txtReadbox";
+            this.txtReadbox.Size = new System.Drawing.Size(800, 21);
+            this.txtReadbox.TabIndex = 10;
+            // 
+            // Image_win
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.txtReadbox);
+            this.Controls.Add(this.btnTest1);
+            this.Controls.Add(this.btnEdge);
+            this.Controls.Add(this.btnBin);
+            this.Controls.Add(this.btnGray);
+            this.Controls.Add(this.btnBlur);
+            this.Controls.Add(this.picResult);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.picOriginal);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Image_win";
+            this.Text = "Image_method";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.picOriginal)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picResult)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picOriginal;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 열기ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem MenuOpenImg;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.PictureBox picResult;
+        private System.Windows.Forms.Button btnBlur;
+        private System.Windows.Forms.Button btnGray;
+        private System.Windows.Forms.Button btnBin;
+        private System.Windows.Forms.Button btnEdge;
+        private System.Windows.Forms.Button btnTest1;
+        private System.Windows.Forms.TextBox txtReadbox;
+    }
+}
+
---
+++ open0322/Image_window.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using OpenCvSharp;
+using Tesseract;
+
+namespace open0322
+{
+    public partial class Image_win : Form
+    {
+        // 현재 열어둔 파일 주소
+        string NowImagePath;
+
+        // 현재의 이미지(800*800)
+        Bitmap NowImg;
+
+        // 처리가 가해진 이미지
+        Bitmap processedImg;
+
+        public Image_win()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuOpenImg_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "이미지 파일(png, jpg)|*.png;*.jpg"; // open 필터 설정
+
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                NowImagePath = open.FileName; // NowImagePath를 초기화
+                Bitmap Image = new Bitmap(NowImagePath); // 비트맵 타입 변수 Image 선언 및 초기화
+                                                         // Mat Image = Cv2.ImRead(NowImagePath); // Mat 선언 및 초기화
+
+
+                /* 0322 : 이미지 크기 변경을 수정해야함. 정사각 화면에 띄우기 위해 이미지를 자르는 방안도 고려해야 할 것 같음. */
+                NowImg = Method.ResizeImage(Image, 800, 800); // 이미지 크기 변경
+                this.picOriginal.Image = NowImg; // 창에 이미지 설정
+
+            }
+        }
+
+        /* this.picOriginal에 설정된 이미지를 Mat 객체로 변환하여 OpenCv 메서드로 처리한 후 picResult에 재설정  */
+        /* picResult에 이미지가 있으면 해당 이미지를 변환하고, 없으면 picOriginal에 설정된 이미지를 변환 : 메서드로 만들 수 있을듯*/
+
+        /* 1)이미지 파일로 저장
+            Bitmap bmp = (Bitmap)pictureBox1.Image;
+            bmp.Save("output.png", ImageFormat.Jpeg);
+        2): 이미지를 byte[] 로 변경
+            ImageConverter converter = new ImageConverter();
+            imageBytes = (byte[])converter.ConvertTo(pictureBox1.Image, typeof(byte[]));*/
+
+/*        public Mat SelectImg()
+        {
+            Mat temp = new Mat();
+            *//* 이미지 선택 메서드, 메모리 누수 방지를 위한 using 사용 *//*
+            if (processedImg is null)
+            {
+                temp = OpenCvSharp.Extensions.BitmapConverter.ToMat(NowImg);
+            } else
+            {
+                temp = OpenCvSharp.Extensions.BitmapConverter.ToMat(processedImg);
+            }
+            return temp;
+        }
+*/
+        public Bitmap SelectImg()
+        {
+            /* 이미지 선택 메서드, 메모리 누수 방지를 위한 using 사용 */
+            if (processedImg is null)
+            {
+                return NowImg;
+            }
+            else
+            {
+                return processedImg;
+            }
+        }
+
+
+        private void btnBlur_Click(object sender, EventArgs e)
+        {
+            Mat img = OpenCvSharp.Extensions.BitmapConverter.ToMat(SelectImg()); // 편집할 이미지
+            Mat result = new Mat(); // 편집 후 이미지
+
+            /* 가우시안 */
+            Cv2.GaussianBlur(img, result, new OpenCvSharp.Size(3, 3), 0);
+            // 편집 후 이미지 할당
+            processedImg = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(result);
+            this.picResult.Image = processedImg;
+
+            // 메모리 해제
+            img.Dispose();
+            result.Dispose();
+        }
+
+        private void btnGray_Click(object sender, EventArgs e)
+        {
+            Mat img = OpenCvSharp.Extensions.BitmapConverter.ToMat(SelectImg()); // 편집할 이미지
+            Mat result = new Mat(); // 편집 후 이미지
+
+            /* 그레이 스케일 : 이미 그레이스케일을 적용한 이미지에는 그레이스케일 불가 */
+            if(img.Channels() != 1)
+            {
+                Cv2.CvtColor(img, result, ColorConversionCodes.BGR2GRAY);
+
+                // 편집 후 이미지 할당
+                processedImg = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(result);
+                this.picResult.Image = processedImg;
+
+                // 메모리 해제
+                img.Dispose();
+                result.Dispose();
+
+            }
+        }
+
+        private void btnBin_Click(object sender, EventArgs e)
+        {
+            Mat img = OpenCvSharp.Extensions.BitmapConverter.ToMat(SelectImg()); // 편집할 이미지
+            Mat result = new Mat(); // 편집 후 이미지
+
+            /* 이진화 : 그레이스케일이 적용되지 않은 이미지는 이진화 하지 않음 */
+            if (img.Channels() == 1)
+            {
+                Cv2.AdaptiveThreshold(img, result, 255, AdaptiveThresholdTypes.GaussianC, ThresholdTypes.Binary, 15, 2);
+                // 편집 후 이미지 할당
+                processedImg = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(result);
+                this.picResult.Image = processedImg;
+
+                // 메모리 해제
+                img.Dispose();
+                result.Dispose();
+            }
+        }
+
+        private void btnEdge_Click(object sender, EventArgs e)
+        {
+            Mat img = OpenCvSharp.Extensions.BitmapConverter.ToMat(SelectImg()); // 편집할 이미지
+            Mat result = new Mat(); // 편집 후 이미지
+
+            /* 경계 추출 */
+            Cv2.Canny(img, result, 100, 200, 3);
+            // 편집 후 이미지 할당
+            processedImg = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(result);
+            this.picResult.Image = processedImg;
+
+            // 메모리 해제
+            img.Dispose();
+            result.Dispose();
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            /* 초기화 */
+            processedImg = null;
+            this.picResult.Image = null;
+            this.txtReadbox.Text = null;
+        }
+
+        private void btnTest1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("인식 버튼 클릭");
+
+            /* 글자 인식 */
+            try
+            {
+                using (var engine = new TesseractEngine(@"./tessdata", "kor", EngineMode.Default))
+                using (Pix pix = PixConverter.ToPix(SelectImg()))
+                using (var page = engine.Process(pix))
+                {
+                    this.txtReadbox.Text = page.GetText();
+                    Console.WriteLine(page.GetText());
+                }
+            }
+            catch (Exception ex)
+            {
+                this.txtReadbox.Text = ex.ToString();
+                Console.WriteLine(ex.ToString());
+            }
+            
+        }
+    }
+}
